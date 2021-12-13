a75f29d72a6b7a95e423ab6de55f9d6e50a5220e
BANGALORE SUDHIR <pb184f@att.com>
2021-12-13 15:24:11 -0800

    Added new features in SignUp page

--- src/main/java/com/automationpractice/qa/pages/SignUpPage.java
+++ src/main/java/com/automationpractice/qa/pages/SignUpPage.java
@@ -1,7 +1,19 @@
 package com.automationpractice.qa.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
 import com.automationpractice.qa.base.TestBase;
 
 public class SignUpPage extends TestBase{
+	
+	
+	//Initializing the Page Objects
+	public SignUpPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String validateSignUpPageTitle() {
+		return driver.getTitle();
+	}
 
 }
